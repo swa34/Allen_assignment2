a09205eb7bf3c5232e39e4c5054e2e39ece48d96
Scott Allen <71349891+swa34@users.noreply.github.com>
2021-09-23 22:58:12 -0400

    Create SortedLinkedList.h

---
+++ SortedLinkedList.h
@@ -0,0 +1,36 @@
+#ifndef SORTEDLINKEDLIST_H
+#define SORTEDLINKEDLIST_H
+
+#include "ItemType.h"
+#include <iostream>
+
+using namespace std;
+
+struct NodeType{
+  ItemType info;
+  NodeType* next;
+};
+
+class LinkedList{
+
+  int length;
+  NodeType* head;
+  NodeType* current;
+  NodeType* findNode(ItemType &item);
+  void getNextNode(); 
+  
+ public:
+  LinkedList();
+  ~LinkedList();
+  int lengthIs()const;
+  void retrieveItem(ItemType &item, bool &found);
+  void insertItem(ItemType &item);
+  void deleteItem(ItemType &item);
+  void resetList();
+  void getNextItem(ItemType &item);
+  void makeEmpty();
+  void print();
+};
+
+
+#endif
